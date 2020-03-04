e2cdb4647d131265c4a304bb4b632e000e8588f8
polina <greenowl403@gmail.com>
2020-03-04 19:35:22 +0300

    create const field SettingsFileName

--- Backup/Program.cs
+++ Backup/Program.cs
@@ -3,12 +3,13 @@ using BackupFile.Settings;
 
 namespace BackupFile
 {
-    
     internal static class Program
     {
+        public const string SettingsFileName = @"D:\ITMO\Разработка ПО\lab1\BackupFile\Backup\Settings.json";
+            
         public static void Main()
         {
-            var settings = new SettingsLoader().Load(@"D:\ITMO\Разработка ПО\lab1\BackupFile\Backup\Settings.json");
+            var settings = new SettingsLoader().Load(SettingsFileName);
 
             var fullPath = DirectoryCreator.CreateWithTimeMark(settings.TargetDirectory);
 
